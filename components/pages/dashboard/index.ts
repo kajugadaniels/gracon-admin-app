efe8e2a01f4b06867fbb064a1035da8b664729bb
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-03-23 15:17:42 +0200

    chore(admin-app): add barrel export for dashboard components

---
+++ components/pages/dashboard/index.ts
@@ -0,0 +1,6 @@
+export { StatCard } from './StatCard';
+export { StatsGrid } from './StatsGrid';
+export { ActivityChart } from './ActivityChart';
+export { SecurityAlert } from './SecurityAlert';
+export { RecentActivity } from './RecentActivity';
+export { VerificationSummary } from './VerificationSummary';
