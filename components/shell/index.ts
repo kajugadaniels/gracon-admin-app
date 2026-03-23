ee55e996943f6cdb9f199b7df726ed8ed666c38e
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-03-23 14:06:18 +0200

    chore(admin-app): add barrel export for shell components

---
+++ components/shell/index.ts
@@ -0,0 +1,4 @@
+export { AdminSidebar }  from './AdminSidebar';
+export { AdminHeader }   from './AdminHeader';
+export { AuthProvider }  from './AuthProvider';
+export { PageHeader }    from './PageHeader';
