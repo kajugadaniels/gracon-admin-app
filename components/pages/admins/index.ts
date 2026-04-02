1581d5f56f2f32e2a533415129b06e8be75d30bc
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-04-02 16:57:57 +0200

    chore(admin-app): add barrel export for admins components

---
+++ components/pages/admins/index.ts
@@ -0,0 +1,4 @@
+export { AdminTable }       from './AdminTable';
+export type { AdminRow }    from './AdminTable';
+export { CreateAdminModal } from './CreateAdminModal';
+export { AdminsClient }     from './AdminsClient';
