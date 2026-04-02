f6c5d41fc0946edcc2bec3d76c797342b4329d69
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-04-02 16:40:18 +0200

    chore(admin-app): add barrel export for security-events components

---
+++ components/pages/security-events/index.ts
@@ -0,0 +1,6 @@
+export { ThreatBadge }                    from './ThreatBadge';
+export { SecurityEventFilters }           from './SecurityEventFilters';
+export type { SecurityEventFilterState }  from './SecurityEventFilters';
+export { DEFAULT_SECURITY_EVENT_FILTERS } from './SecurityEventFilters';
+export { SecurityEventTable }             from './SecurityEventTable';
+export { SecurityEventsClient }           from './SecurityEventsClient';
