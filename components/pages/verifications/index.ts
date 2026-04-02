3525cf1c9c5058f7beb4601cc5997d352b83bd8a
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-04-02 14:16:21 +0200

    chore(admin-app): add barrel export for verifications components

---
+++ components/pages/verifications/index.ts
@@ -0,0 +1,7 @@
+export { VerificationFilters }         from './VerificationFilters';
+export type { VerificationFilterState } from './VerificationFilters';
+export { DEFAULT_VERIFICATION_FILTERS } from './VerificationFilters';
+export { VerificationTable }           from './VerificationTable';
+export { VerificationsClient }         from './VerificationsClient';
+export { ScoreBreakdown }              from './ScoreBreakdown';
+export { VerificationDetailClient }    from './VerificationDetailClient';
